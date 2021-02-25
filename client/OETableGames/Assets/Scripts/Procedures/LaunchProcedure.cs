68990aaea39e038a6086a025b55e349770135ce8
dongrunbin <476235060@qq.com>
2021-02-25 12:40:38 +0100

    create the first procedure to test

---
+++ client/OETableGames/Assets/Scripts/Procedures/LaunchProcedure.cs
@@ -0,0 +1,17 @@
+//===================================================
+//Author      : DRB
+//CreateTime  ：2021/2/25 12:19:12
+//Description ：
+//===================================================
+using DrbFramework.Procedure;
+
+public class LaunchProcedure : Procedure
+{
+    public override void OnEnter(object userData)
+    {
+        base.OnEnter(userData);
+
+
+    }
+}
+
