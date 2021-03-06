aa2e7d48f479627f0f3632c56a211f2529af8730
dongrunbin <476235060@qq.com>
2021-03-07 00:18:04 +0100

    add web url

---
+++ client/OETableGames/Assets/Scripts/Common/ConstDefine.cs
@@ -0,0 +1,10 @@
+//===================================================
+//Author      : DRB
+//CreateTime  ：2021/3/6 21:32:06
+//Description ：
+//===================================================
+
+public class ConstDefine
+{
+    public static string WebUrl = "";
+}
